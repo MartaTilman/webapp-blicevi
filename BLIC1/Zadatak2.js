1200616a04f69e46b13b4829abc73f6183f274ae
MartaTilman <75136778+MartaTilman@users.noreply.github.com>
2022-10-11 08:41:41 +0200

    rjeseni zadatak2

--- BLIC1/Zadatak2.js
+++ BLIC1/Zadatak2.js
@@ -1,10 +1,11 @@
-var str = "marta"
+var str = "javascript i nije toliko los"
 
 function vrati(x) {
     var b = x / 2
     var a = Math.ceil(b)
-    for (let index = 0; index < array.length; index++) {
-        const element = array[index];
+    for (let index = x.a; index < x.length; index++) {
+        console.log(x.index)
 
     }
 }
+vrati(str)
